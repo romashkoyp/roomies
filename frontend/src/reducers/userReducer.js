a8b203d31a368b90902f811bc104bf01942693c7
Yaroslav <romashkoyp@gmail.com>
2024-09-13 14:49:28 +0300

    commenting redundant code

--- frontend/src/reducers/userReducer.js
+++ frontend/src/reducers/userReducer.js
@@ -1,8 +1,8 @@
 import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = {
-  username: '',
-  password: '',
+  // username: '',
+  // password: '',
   user: null
 }
 
@@ -10,15 +10,16 @@ const userSlice = createSlice({
     name: 'user',
     initialState,
     reducers: {
-    setUsername(state, action) { state.username = action.payload },
-    setPassword(state, action) { state.password = action.payload },
+    // setUsername(state, action) { state.username = action.payload },
+    // setPassword(state, action) { state.password = action.payload },
     setUser(state, action) { state.user = action.payload }
     }
 })
 
-export const { setUsername, setPassword, setUser } = userSlice.actions
-export const selectUsername = (state) => state.user.username
-export const selectPassword = (state) => state.user.password
+export const { setUser } = userSlice.actions
+// export const { setUsername, setPassword, setUser } = userSlice.actions
+// export const selectUsername = (state) => state.user.username
+// export const selectPassword = (state) => state.user.password
 export const selectUser = (state) => state.user.user
 
 export default userSlice.reducer
