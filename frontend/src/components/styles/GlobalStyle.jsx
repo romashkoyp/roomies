3f67c8ad2e1e7c379306ffd8f7ca39af54dd7033
Yaroslav <romashkoyp@gmail.com>
2024-09-13 14:44:21 +0300

    update styles and add new one for table view

--- frontend/src/components/styles/GlobalStyle.jsx
+++ frontend/src/components/styles/GlobalStyle.jsx
@@ -2,7 +2,7 @@ import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyles = createGlobalStyle`
   body {
-    margin: 3rem;
+    margin: 1.5rem;
     padding: 0;
     font-family: 'Roboto', sans-serif;
     color: #333;
@@ -28,6 +28,30 @@ const GlobalStyles = createGlobalStyle`
       //text-decoration: underline; /* Add underline on hover */
     }
   }
+
+  table, th, td {
+    border: 1px solid grey;
+    border-collapse: collapse;
+  }
+
+  table {
+    margin: auto;
+    width: 90%;
+  }
+
+  th, td {
+    padding: 5px;
+  }
+
+  tbody {
+    tr {
+      &:hover {
+        background-color: #ccdae8;
+        transition: color 0.3s ease;
+      }
+    }
+  }
+
 `
 
 export default GlobalStyles
--- frontend/src/components/styles/Wrapper.jsx
+++ frontend/src/components/styles/Wrapper.jsx
@@ -1,11 +1,12 @@
 import styled from 'styled-components'
 
 const Wrapper = styled.div`
-  margin: 3%;
-  width: 50%;
-  padding: 24px 24px;
+  margin-top: 20px;
+  width: 800px;
+  max-width: 90%;
+  padding: 20px 20px;
   border-radius: 20px;
-  box-shadow: 0 4px 12px rgba(0, 0, 0, 0.5);
+  box-shadow: 0 4px 12px rgba(0, 0, 0, 0.3);
 `
 
 export default Wrapper
