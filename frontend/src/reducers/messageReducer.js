2d9f2e236f37b186aed1be5ab63f80fdc691713e
Yaroslav <romashkoyp@gmail.com>
2024-09-13 14:42:05 +0300

    enhance reducer for messages

--- frontend/src/reducers/messageReducer.js
+++ frontend/src/reducers/messageReducer.js
@@ -1,13 +1,45 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import messageService from '../services/message'
+
+export const fetchMessages = createAsyncThunk(
+  'messages/fetchAll',
+  async (_, { rejectWithValue }) => {
+    const result = await messageService.getAllMessages()
+    if (result.success) {
+      return result.data
+    } else {
+      return rejectWithValue(result.error)
+    }
+  }
+)
 
 const messagesSlice = createSlice({
   name: 'messages',
   initialState: [],
   reducers: {
-    setMessages(state, action) { return action.payload }
+    setMessages(state, action) {
+      return action.payload
+    },
+    addMessage(state, action) {
+      state.push(action.payload)
+    },
+    updateMessage(state, action) {
+      const index = state.findIndex(message => message.id === action.payload.id)
+      if (index !== -1) {
+        state[index] = action.payload
+      }
+    },
+    deleteMessage(state, action) {
+      return [...state.filter(message => message.id !== action.payload)] 
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(fetchMessages.fulfilled, (state, action) => {
+      return action.payload
+    })
   }
 })
 
-export const { setMessages } = messagesSlice.actions
+export const { setMessages, addMessage, updateMessage, deleteMessage } = messagesSlice.actions
 export const selectMessages = (state) => state.messages
 export default messagesSlice.reducer
