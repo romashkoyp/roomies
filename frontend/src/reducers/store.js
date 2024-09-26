b7d83adb42d1f046068958ff74ce54c0e2f4ca7e
Yaroslav <romashkoyp@gmail.com>
2024-09-26 20:46:06 +0300

    add individual dates to reducer

--- frontend/src/reducers/store.js
+++ frontend/src/reducers/store.js
@@ -3,13 +3,15 @@ import userReducer from './userReducer'
 import notificationReducer from './notificationReducer'
 import messageReducer from './messageReducer'
 import roomReducer from './roomReducer'
+import individualDateReducer from './individualDateReducer'
 
 const store = configureStore({
     reducer: {
       users: userReducer,
       notification: notificationReducer,
       messages: messageReducer,
-      rooms: roomReducer
+      rooms: roomReducer,
+      individualDates: individualDateReducer,
     }
   })
 
