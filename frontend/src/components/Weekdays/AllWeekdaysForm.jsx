359ca754d87b04fd75fa3645cfe62050b08d19e4
Yaroslav <romashkoyp@gmail.com>
2024-09-27 18:49:49 +0300

    add form for correcting weekdays

---
+++ frontend/src/components/Weekdays/AllWeekdaysForm.jsx
@@ -0,0 +1,116 @@
+import { useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import Wrapper from '../styles/Wrapper'
+import { PrimaryButton, SecondaryButton } from '../styles/Buttons'
+import { setNotification } from '../../reducers/notificationReducer'
+import { selectUser } from '../../reducers/userReducer'
+import weekdaysService from '../../services/weekday'
+import { updateGlobalWeekdays, fetchWeekdays } from '../../reducers/weekdayReducer'
+import LinkHeader from '../styles/LinkHeader'
+
+const AllWeekdaysForm = () => {
+  const dispatch = useDispatch()
+  const user = useSelector(selectUser)
+  const [formData, setFormData] = useState({
+    availability: true,
+    time_begin: '09:00',
+    time_end: '17:00',
+  })
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    const result = await weekdaysService.updateGlobalWeekdays(formData)
+
+    if (result.success) {
+      dispatch(updateGlobalWeekdays(result.data))
+      dispatch(fetchWeekdays())
+      dispatch(setNotification('Weekdays updated successfully!', 'success', 5))
+      setFormData({
+        availability: true,
+        time_begin: '09:00',
+        time_end: '17:00',
+      })
+      setIsVisible(false)
+    } else {
+      dispatch(setNotification(result.error, 'error', 5))
+    }
+  }
+
+  const handleChange = (event) => {
+    const { name, value, type, checked } = event.target
+    setFormData(prevData => ({ ...prevData, [name]: type === 'checkbox' ? checked : value }))
+  }
+
+  const [isVisible, setIsVisible] = useState(false)
+
+  const handleClick = () => {
+    setIsVisible(!isVisible)
+  }
+
+  const handleDeleteWeekdays = async (event) => {
+    event.preventDefault()
+    if (confirm("This action restored all weekdays settings to default values. Are you sure to continue?")) {    
+      const result = await weekdaysService.deleteWeekdays()
+      if (result.success) {
+        dispatch(fetchWeekdays())
+        setIsVisible(false)
+        dispatch(setNotification('Global weekdays settings restored', 'success', 5))
+      } else {
+        dispatch(fetchWeekdays())
+        dispatch(setNotification(result.error, 'error', 5))
+      }
+    } else return null
+  }
+
+  if (user === undefined || !user.admin || !user.enabled) { 
+    return null
+  }
+
+  return (
+    <Wrapper>
+      <LinkHeader onClick={handleClick}><h3>Edit global weekdays for all rooms</h3></LinkHeader>
+      {isVisible && (
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="availability">Availability:</label>
+            <input
+              type="checkbox"
+              id="availability"
+              name="availability"
+              checked={formData.availability}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="time_begin">Time Begin:</label>
+            <input
+              type="time"
+              id="time_begin"
+              name="time_begin"
+              value={formData.time_begin}
+              onChange={handleChange}
+              disabled={!formData.availability}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="time_end">Time End:</label>
+            <input
+              type="time"
+              id="time_end"
+              name="time_end"
+              value={formData.time_end}
+              onChange={handleChange}
+              disabled={!formData.availability}
+            />
+          </div>
+          <>
+            <PrimaryButton type="submit">Submit</PrimaryButton>
+            <SecondaryButton onClick={handleDeleteWeekdays}>Restore</SecondaryButton>
+          </>
+        </form>
+      )}
+    </Wrapper>
+  )
+}
+
+export default AllWeekdaysForm
