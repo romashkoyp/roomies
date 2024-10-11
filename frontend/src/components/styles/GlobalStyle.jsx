7315122d2c43e67c1afd12295a941186fe77c26d
Yaroslav <romashkoyp@gmail.com>
2024-10-11 22:44:03 +0300

    add styles for pop up form

--- frontend/src/components/styles/GlobalStyle.jsx
+++ frontend/src/components/styles/GlobalStyle.jsx
@@ -67,6 +67,28 @@ const GlobalStyles = createGlobalStyle`
     align-items: center;
     margin-bottom: 15px;
   }
+  
+  .booking-form-popup {
+    position: fixed; 
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: white;
+    padding: 20px;
+    border-radius: 5px;
+    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.5);
+    z-index: 1000;
+  }
+
+  .overlay {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5); 
+    z-index: 999; 
+  }
 
   label {
     width: 160px;
