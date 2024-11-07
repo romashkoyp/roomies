416027839352b9360288bd2c1a3abfb0bb337ff2
Yaroslav <romashkoyp@gmail.com>
2024-11-07 13:54:19 +0200

    add dropdown functionality

--- frontend/src/components/Menu.jsx
+++ frontend/src/components/Menu.jsx
@@ -1,4 +1,4 @@
-import { NavLink } from 'react-router-dom'
+import { NavLink, useLocation } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import signinService from '../services/signin'
 import { setNotification } from '../reducers/notificationReducer'
@@ -48,10 +48,62 @@ const NavLinkStyledSignOut = styled(NavLink)`
   }
 `
 
+const LinkDropDown = styled(NavLink)`
+  display: inline-block;
+  text-decoration: none;
+  padding: 10px 15px;
+  color: #333;
+  border-radius: 5px;
+  transition: background-color 0.3s ease;
+
+  &:hover {
+    background-color: #B1D4E0;
+  }
+
+  &.active {
+    background-color: #1B263B;
+    color: white;
+  }
+`
+
+const DropdownContainer = styled.div`
+  position: relative;
+  display: inline-block;
+
+  &:hover .dropdown-content {
+    display: block;
+  }
+`
+
+const DropdownContent = styled.div`
+  display: none;
+  position: absolute;
+  background-color: #f9f9f9;
+  min-width: 160px;
+  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+  z-index: 1;
+`
+
+const DropdownItem = styled(NavLink)`
+  color: black;
+  padding: 12px 16px;
+  text-decoration: none;
+  display: block;
+
+  &:hover {
+    background-color: #f1f1f1;
+  }
+
+  &.active {
+    background-color: #B1D4E0;
+  }
+`
+
 const Menu = () => {
   const user = useSelector((state) => state.users.user)
-  const date = new Date().toISOString().slice(0, 10)
   const dispatch = useDispatch()
+  const location = useLocation()
+  const isSettingsActive = location.pathname.startsWith('/settings')
 
   const handleSignout = async () => {
     if (confirm("Are you sure?")) {
@@ -75,17 +127,40 @@ const Menu = () => {
             <>
               <NavLinkStyled to="/notifications">Messages</NavLinkStyled>
               <NavLinkStyled to="/users">Users</NavLinkStyled>
-              <NavLinkStyled to="/rooms/dates">dates</NavLinkStyled>
-              <NavLinkStyled to="/settings/weekdays">Weekdays</NavLinkStyled>
-              <NavLinkStyled to="/settings/dates">Dates</NavLinkStyled>
+              
+              <DropdownContainer>
+                <LinkDropDown 
+                  to="/settings/weekdays"
+                  className={isSettingsActive ? "active" : ""}>
+                    Settings
+                </LinkDropDown>
+                <DropdownContent className="dropdown-content">
+                  <DropdownItem to="/settings/weekdays">Weekdays</DropdownItem>
+                  <DropdownItem to="/settings/dates">Holidays</DropdownItem>
+                </DropdownContent>
+              </DropdownContainer>
             </>
           )}
-          {user &&
+
+          {user?.admin && (
+            <>
+              <DropdownContainer>
+                <LinkDropDown to="/rooms">Rooms</LinkDropDown>
+                <DropdownContent className="dropdown-content">
+                  <DropdownItem to="/rooms/dates">Individual dates</DropdownItem>
+                </DropdownContent>
+              </DropdownContainer>
+              <NavLinkStyled to="/bookings">Bookings</NavLinkStyled>
+            </>
+          )}
+
+          {user && !user.admin && (
             <> 
+              <NavLinkStyled to="/notifications">Messages</NavLinkStyled>
               <NavLinkStyled to="/rooms">Rooms</NavLinkStyled>
               <NavLinkStyled to="/bookings">Bookings</NavLinkStyled>
             </>
-          }
+          )}
       </NavList>
       <NavList>
         {user ? (
