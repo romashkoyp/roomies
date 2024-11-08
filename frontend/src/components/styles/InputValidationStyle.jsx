974ac0d853ae806a120ee84bed3ae6448192af07
Yaroslav <romashkoyp@gmail.com>
2024-11-08 20:04:46 +0200

    add style for input

---
+++ frontend/src/components/styles/InputValidationStyle.jsx
@@ -0,0 +1,23 @@
+import styled from 'styled-components'
+
+const StyledInput = styled.input.withConfig({
+  shouldForwardProp: (prop) => prop !== 'hasError',
+})`
+  flex-grow: 1;
+  max-width: 300px;
+  padding: 10px;
+  font-size: 16px;
+  border: 1px solid ${(props) => (props.hasError ? '#ff5c5c' : '#ccc')};
+  border-radius: 4px;
+  outline: none;
+
+  &:focus {
+    border-color: ${(props) => (props.hasError ? '#ff5c5c' : '#007bff')};
+    box-shadow: ${(props) =>
+      props.hasError
+        ? '0 0 5px rgba(220, 53, 69, 0.5)'
+        : '0 0 5px rgba(0, 123, 255, 0.5)'};
+  }
+`
+
+export default StyledInput
