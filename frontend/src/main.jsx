3b6e67050ae77918dcb72e969cc2dcbc40d75fa4
Yaroslav <romashkoyp@gmail.com>
2024-12-07 16:48:00 +0200

    update

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { BrowserRouter as Router } from 'react-router-dom'
 
-import App from './App'
+import App from './App.jsx'
 import GlobalStyles from './components/styles/GlobalStyle'
 import store from './reducers/store'
 
