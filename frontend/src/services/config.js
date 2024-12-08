bfd4b3c0ea2875517277e408447c31a7e604db9c
Yaroslav <romashkoyp@gmail.com>
2024-12-08 11:50:54 +0200

    configure build fro production

--- frontend/src/services/config.js
+++ frontend/src/services/config.js
@@ -1,4 +1,9 @@
 // eslint-disable-next-line no-undef
-const BASE_URL = process.env.BASE_API_URL
+
+// address for development process only
+// const BASE_URL = process.env.BASE_API_URL
+
+// address for production
+const BASE_URL = import.meta.env.PROD ? 'https://roomies-backend-2yba.onrender.com/api' : '/api'
 
 export default BASE_URL
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,14 @@
 import react from '@vitejs/plugin-react'
-import dotenv from 'dotenv'
+// import dotenv from 'dotenv'
 import { defineConfig } from 'vite'
 
-dotenv.config()
+// dotenv.config()
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  define: {
-    // eslint-disable-next-line no-undef
-    'process.env': process.env
-  }
+  plugins: [react()]
+  // define: {
+  //   // eslint-disable-next-line no-undef
+  //   'process.env': process.env
+  // }
 })
