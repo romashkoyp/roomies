c0df7710e695b655e18c13eafb9598b3f4de0411
Yaroslav <romashkoyp@gmail.com>
2024-12-08 18:22:30 +0200

    update vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,7 @@
 import react from '@vitejs/plugin-react'
-// import dotenv from 'dotenv'
 import { defineConfig } from 'vite'
-
-// dotenv.config()
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // define: {
-  //   // eslint-disable-next-line no-undef
-  //   'process.env': process.env
-  // }
 })
