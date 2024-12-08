646bb2072a963b86ee8a54525ad4e7355b88c878
Yaroslav <romashkoyp@gmail.com>
2024-12-08 20:07:17 +0200

    change eslint rules

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -19,7 +19,7 @@ export default [
     },
     rules: {
       '@stylistic/js/indent': ['error', 2],
-      '@stylistic/js/linebreak-style': ['error', 'windows'],
+      '@stylistic/js/linebreak-style': 'off',
       '@stylistic/js/quotes': ['error', 'single'],
       '@stylistic/js/semi': ['error', 'never'],
       'eqeqeq': 'error',
