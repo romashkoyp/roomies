6e96f55c20eb43936ee58cc979a40929f28566a5
Yaroslav <romashkoyp@gmail.com>
2025-04-20 23:09:07 +0300

    update vite configuration to include server and build options

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,14 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    historyApiFallback: true,
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
 })
